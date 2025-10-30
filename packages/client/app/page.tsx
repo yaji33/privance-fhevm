62edb0499512a00d25e02dd1e0fdedabeff770c5
Karel_Jhona <159889662+jhona06@users.noreply.github.com>
2025-10-31 01:10:23 +0800

    feat: added marketplace

--- packages/client/app/page.tsx
+++ packages/client/app/page.tsx
@@ -5,21 +5,22 @@ import Navbar from "./_components/layout/Navbar";
 import HomePage from "./_components/dashboard/Home";
 import BorrowPage from "./_components/dashboard/Borrow";
 import LenderPage from "./_components/dashboard/Lender";
-import HowItWorksPage from "./_components/dashboard/How_it_works";
+import MarketplacePage from "./_components/dashboard/Marketplace";
+
 
 export default function Dashboard() {
-  const [activePage, setActivePage] = useState<"Home" | "Borrow" | "Lender" | "HowItWorks">("Home");
+  const [activePage, setActivePage] = useState<"Home" | "Borrow" | "Lender" | "Marketplace">("Home");
 
   const renderPage = () => {
     switch (activePage) {
       case "Home":
         return <HomePage />;
       case "Borrow":
-        return <BorrowPage />;
+        return <BorrowPage />;  
       case "Lender":
         return <LenderPage />;
-      case "HowItWorks":
-        return <HowItWorksPage />;
+      case "Marketplace":
+        return <MarketplacePage />;
       default:
         return <HomePage />;
     }
